2025bbb36c1673015c2bf28a6dd94ac9bedc5932
DrSheppard <60893791+Drsheppard01@users.noreply.github.com>
2025-12-04 01:16:05 +0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,13 +6,30 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm \
+  base-devel \
+  cmake \
+  libarchive \
+  sdl3 \
+  sdl3_image \
+  sdl3_ttf \
+  libinih \
+  harfbuzz \
+  spdlog \
+  fmt \
+  libxml2
+
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+echo "Getting big-launcher source code and assets.."
+echo "---------------------------------------------------------------"
+git clone https://github.com/complexlogic/big-launcher.git
+wget https://github.com/complexlogic/big-launcher/files/10326572/assets.zip
+
+echo "Start building big-launcher.."
+echo "---------------------------------------------------------------"
 
 # If the application needs to be manually built that has to be done down here
